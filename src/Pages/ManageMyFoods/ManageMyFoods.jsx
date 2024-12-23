caf5f5d938d467aac33696dde0298e0899efb451
MukitHossen7 <hossenmukit7@gmail.com>
2024-12-23 17:01:06 +0600

    delete functionlity add in manage my food page

--- src/Pages/ManageMyFoods/ManageMyFoods.jsx
+++ src/Pages/ManageMyFoods/ManageMyFoods.jsx
@@ -3,20 +3,46 @@ import { useContext, useEffect, useState } from "react";
 
 import { FaEdit, FaTrash } from "react-icons/fa";
 import { AuthContext } from "../../Provider/AuthProvider";
+import Swal from "sweetalert2";
 
 const ManageMyFoods = () => {
   const { user } = useContext(AuthContext);
   const [myFoods, setMyFoods] = useState([]);
   useEffect(() => {
-    axios
-      .get(
+    const manageMyFood = async () => {
+      const { data } = await axios.get(
         `${import.meta.env.VITE_BASE_URL}/manage-my-foods?email=${user?.email}`
-      )
-      .then((response) => {
-        setMyFoods(response.data);
-      });
+      );
+      setMyFoods(data);
+    };
+    manageMyFood();
   }, [user?.email]);
-
+  const handleDeleteMyFoods = async (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios
+          .delete(`${import.meta.env.VITE_BASE_URL}/all-foods/${id}`)
+          .then((res) => {
+            if (res.data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your food has been deleted.",
+                icon: "success",
+              });
+              setMyFoods(myFoods.filter((food) => food._id !== id));
+            }
+          });
+      }
+    });
+  };
   return (
     <div>
       <div className="pt-10 pb-20 w-11/12 md:w-11/12 lg:w-11/12 xl:container mx-auto">
@@ -28,12 +54,12 @@ const ManageMyFoods = () => {
             <table className="table-auto w-full text-left">
               <thead>
                 <tr className="bg-teal-500 text-white">
-                  <th className="px-4 py-2">Food Image</th>
+                  <th className="px-4 py-2">Image</th>
                   <th className="px-4 py-2">Food Name</th>
                   <th className="px-4 py-2">Quantity</th>
                   <th className="px-4 py-2">Pickup Location</th>
-                  <th className="px-4 py-2">Expired Date</th>
-                  <th className="px-4 py-2">Food Status</th>
+                  <th className="px-4 py-2">Expired</th>
+                  <th className="px-4 py-2">Status</th>
                   <th className="px-4 py-2">Additional Notes</th>
                   <th className="px-4 py-2">Actions</th>
                 </tr>
@@ -60,7 +86,10 @@ const ManageMyFoods = () => {
                       <button className="text-white bg-teal-500 hover:bg-teal-600 px-3 py-1 lg:py-2 rounded-md flex items-center">
                         <FaEdit className="mr-2" /> Update
                       </button>
-                      <button className="text-white bg-red-500 hover:bg-red-600 px-3 py-1 lg:py-2 rounded-md flex items-center">
+                      <button
+                        onClick={() => handleDeleteMyFoods(food._id)}
+                        className="text-white bg-red-500 hover:bg-red-600 px-3 py-1 lg:py-2 rounded-md flex items-center"
+                      >
                         <FaTrash className="mr-2" /> Delete
                       </button>
                     </td>
