74a134b7e2c7ca7fea7e331a5086a5c4975c6382
MukitHossen7 <hossenmukit7@gmail.com>
2024-12-24 18:02:46 +0600

    some problem slove in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,8 +14,8 @@ const Footer = () => {
             d="M0 22L120 16.7C240 11 480 1.00001 720 0.700012C960 1.00001 1200 11 1320 16.7L1440 22V54H1320C1200 54 960 54 720 54C480 54 240 54 120 54H0V22Z"
           />
         </svg>
-        <div className="px-4 pt-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-          <div className="flex flex-col lg:flex-row gap-8 lg:gap-16 row-gap-10 mb-8 items-center text-center lg:text-start lg:items-start justify-between">
+        <div className="pt-12 mx-auto w-11/12 md:w-11/12 lg:w-11/12 xl:container  ">
+          <div className="flex flex-col lg:flex-row gap-8 lg:gap-16 row-gap-10 mb-8 items-center text-center lg:text-start lg:items-start justify-around">
             <div className="md:max-w-md lg:col-span-2">
               <a
                 href="/"
@@ -138,8 +138,8 @@ const Footer = () => {
               </ul>
             </div>
           </div>
-          <div className="flex flex-col justify-between pt-5 pb-10 border-t border-gray-600 sm:flex-row">
-            <p className="text-sm text-gray-200 text-center">
+          <div className="pt-5 pb-10 border-t border-gray-600 sm:flex-row">
+            <p className="text-sm text-gray-200 text-center ">
               © 2026 FoodSharing. All rights reserved.
             </p>
           </div>
