31973457296315664f86bb1a161d0eded3817909
MukitHossen7 <hossenmukit7@gmail.com>
2024-12-24 15:33:25 +0600

    footer design is complete according food sharing project

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <div>
@@ -13,7 +15,7 @@ const Footer = () => {
           />
         </svg>
         <div className="px-4 pt-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-          <div className="grid gap-16 row-gap-10 mb-8 lg:grid-cols-6">
+          <div className="flex flex-col lg:flex-row gap-8 lg:gap-16 row-gap-10 mb-8 items-center text-center lg:text-start lg:items-start justify-between">
             <div className="md:max-w-md lg:col-span-2">
               <a
                 href="/"
@@ -36,7 +38,7 @@ const Footer = () => {
                   <rect x="14" y="1" width="7" height="6" />
                   <rect x="14" y="11" width="7" height="12" />
                 </svg>
-                <span className="ml-3 text-xl font-bold tracking-wide text-white uppercase">
+                <span className="ml-3 text-xl text-center font-bold tracking-wide text-white uppercase">
                   FoodSharing
                 </span>
               </a>
@@ -51,138 +53,95 @@ const Footer = () => {
                 </p>
               </div>
             </div>
-            <div className="grid grid-cols-2 gap-5 row-gap-8 lg:col-span-4 md:grid-cols-4">
-              <div>
-                <p className="font-semibold tracking-wide text-white">
-                  Resources
-                </p>
-                <ul className="mt-2 space-y-2">
-                  <li>
-                    <a
-                      href="/"
-                      className="transition-colors duration-300 hover:text-gray-800"
-                    >
-                      News
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/"
-                      className="transition-colors duration-300 hover:text-gray-800"
-                    >
-                      Blog
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/"
-                      className="transition-colors duration-300 hover:text-gray-800"
-                    >
-                      FAQs
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/"
-                      className="transition-colors duration-300 hover:text-gray-800"
-                    >
-                      Community
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <p className="font-semibold tracking-wide text-white">
-                  Company
-                </p>
-                <ul className="mt-2 space-y-2">
-                  <li>
-                    <a
-                      href="/"
-                      className="transition-colors duration-300 hover:text-gray-800"
-                    >
-                      About Us
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/"
-                      className="transition-colors duration-300 hover:text-gray-800"
-                    >
-                      Careers
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/"
-                      className="transition-colors duration-300 hover:text-gray-800"
-                    >
-                      Contact
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/"
-                      className="transition-colors duration-300 hover:text-gray-800"
-                    >
-                      Partnerships
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <p className="font-semibold tracking-wide text-white">Social</p>
-                <ul className="mt-2 space-y-2">
-                  <li>
-                    <a
-                      href="/"
-                      className="transition-colors duration-300 hover:text-gray-800"
-                    >
-                      Facebook
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/"
-                      className="transition-colors duration-300 hover:text-gray-800"
-                    >
-                      Instagram
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/"
-                      className="transition-colors duration-300 hover:text-gray-800"
-                    >
-                      Twitter
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/"
-                      className="transition-colors duration-300 hover:text-gray-800"
-                    >
-                      LinkedIn
-                    </a>
-                  </li>
-                </ul>
-              </div>
+            <div>
+              <p className="font-semibold tracking-wide text-white">
+                Resources
+              </p>
+              <ul className="mt-2 space-y-2">
+                <li>
+                  <Link
+                    to="/"
+                    className="transition-colors duration-300 hover:text-gray-800"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/availableFoods"
+                    className="transition-colors duration-300 hover:text-gray-800"
+                  >
+                    Available Foods
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/addFood"
+                    className="transition-colors duration-300 hover:text-gray-800"
+                  >
+                    Add Food
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/manageMyFoods"
+                    className="transition-colors duration-300 hover:text-gray-800"
+                  >
+                    Manage My Foods
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/myFoodRequest"
+                    className="transition-colors duration-300 hover:text-gray-800"
+                  >
+                    My Food Request
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <p className="font-semibold tracking-wide text-white">Social</p>
+              <ul className="mt-2 space-y-2">
+                <li>
+                  <a
+                    href="/"
+                    className="transition-colors duration-300 hover:text-gray-800"
+                  >
+                    Facebook
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="transition-colors duration-300 hover:text-gray-800"
+                  >
+                    Instagram
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="transition-colors duration-300 hover:text-gray-800"
+                  >
+                    Twitter
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="transition-colors duration-300 hover:text-gray-800"
+                  >
+                    LinkedIn
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
           <div className="flex flex-col justify-between pt-5 pb-10 border-t border-gray-600 sm:flex-row">
-            <p className="text-sm text-gray-200">
-              © 2024 FoodSharing. All rights reserved.
+            <p className="text-sm text-gray-200 text-center">
+              © 2026 FoodSharing. All rights reserved.
             </p>
-            <div className="flex items-center mt-4 space-x-4 sm:mt-0">
-              <a
-                href="/"
-                className="transition-colors duration-300 text-gray-200 hover:text-gray-800"
-              >
-                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
-                  <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
-                </svg>
-              </a>
-            </div>
           </div>
         </div>
       </footer>
